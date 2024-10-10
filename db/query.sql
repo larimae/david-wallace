63bc9a48d293fbfa64a2cb480520db63103bfa8d
larimae <larissamae60@gmail.com>
2024-10-09 21:39:17 -0700

    finished the query file I think, only 3 in there so hopefully I won't need anymore but we'll see

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,35 @@
-SELECT movies.movie_name AS movie, reviews.review
-FROM reviews
-LEFT JOIN movies
-ON reviews.movie_id = movies.id
-ORDER BY movies.movie_name;
+SELECT * FROM departments;
+
+/* Output: all departments 
++------------+
+| department |
++------------+
+*/
+
+SELECT role.title , department.name, role.salary 
+FROM role
+JOIN department
+ON role.department_id = department.id;
+
+/* Output: all roles 
++--------------+-------------------------------+
+| title       | department        | salary     |
++--------------+-------------------------------+
+*/
+
+SELECT 
+    employee.first_name, 
+    employee.last_name, 
+    role.title, 
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employee
+JOIN role ON employee.role_id = role.id
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
+
+
+
+/* Output: all employees 
++--------------+----------------------------------------+
+| first | last | title  | department | salary | manager |
+|+--------------+---------------------------------------+
+
